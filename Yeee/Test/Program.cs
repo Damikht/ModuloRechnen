76d3bb44788b74de10e72f50983180dc18ab2d45
Dami <Kuhnhardt-Damian@outlook.com>
2025-05-21 22:29:29 +0200

    Add files via upload

---
+++ Yeee/Test/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Modulo rechnen leicht gemacht! (Ergebnis ist der rest aus einer Division)");
+
+int a = 10;
+int b = 3;
+int sum1 = a % b;
+Console.WriteLine("10 (Divident) % 3 (Divisor) = " + sum1);
+
+int c = 14;
+int d = 5;
+int sum2 = c % d;
+Console.WriteLine("14 (Divident) % 5 (Divisor) = " + sum2);
+
+int e = 20;
+int f = 4;
+int sum3 = e % f;
+Console.WriteLine("20 (Divident) % 4 (Divisor) = " + sum3);
+
+int g = 7;
+int h = 7;
+int sum4 = g % h;
+Console.WriteLine("7 (Divident) % 7 (Divisor) = " + sum4);
+
+int i = 19;
+int j = 6;
+int sum5 = i % j;
+Console.WriteLine("19 (Divident) % 6 (Divisor) = " + sum5);
+
+
+Console.WriteLine("Jetzt du!");
+
+int k = 3;     
+Console.WriteLine("23 % 10 = ");
+string input = Console.ReadLine();
+
+if (k == 3)
+{
+Console.WriteLine("Richtig!");
+            
+} 
+else 
+{
+Console.WriteLine("Leider falsch! Die richtige Lösung ist 3.");  
+}
+
+int n = 1;
+Console.WriteLine("17 % 8 = ");
+string inpu = Console.ReadLine();
+
+if (n == 1)
+{
+    Console.WriteLine("Richtig!");
+
+}
+else
+{
+    Console.WriteLine("Leider falsch! Die richtige Lösung ist 1.");
+}
+
+int l = 0;
+Console.WriteLine("8 % 2 = ");
+string inp = Console.ReadLine();
+
+if (l == 0)
+{
+    Console.WriteLine("Richtig!");
+
+}
+else
+{
+    Console.WriteLine("Leider falsch! Die richtige Lösung ist 0.");
+}
+
+int m = 1;
+Console.WriteLine("100 % 33 = ");
+string intp = Console.ReadLine();
+
+if (m == 1)
+{
+    Console.WriteLine("Richtig!");
+
+}
+else
+{
+    Console.WriteLine("Leider falsch! Die richtige Lösung ist 1.");
+}
+
+int o = 8;
+Console.WriteLine("35 % 9 = ");
+string sum = Console.ReadLine();
+
+if (o == 8)
+{
+    Console.WriteLine("Richtig!");
+
+}
+else
+{
+    Console.WriteLine("Leider falsch! Die richtige Lösung ist 1.");
+}
+
+
+
+Console.WriteLine("Wunderbar du weißt jetzt wie man modulo rechnet! :)")
